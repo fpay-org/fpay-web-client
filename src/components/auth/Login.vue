2e34a9c5e42f7a0ca79be64a996744f4f338ef3c
Sashika Nawarathne <cazci269@gmail.com>
2019-09-19 22:59:53 +0530

    Add validation

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -2,8 +2,9 @@
   <div>
     <a @click.stop="dialog = true">Login</a>
 
-    <v-dialog v-model="dialog" max-width="350">
-      <v-card color="blue-grey lighten-5">
+    <v-dialog v-model="dialog" max-width="350" :persistent="isLocked">
+      <!-- Login section -->
+      <v-card color="blue-grey lighten-5" :loading="isLoading" v-if="loginForm">
         <v-card-title class="justify-center">
           <div class="mt-4">Welcome!</div>
         </v-card-title>
@@ -11,35 +12,83 @@
           <div class="mt-4 mx-4">
             <form role="form">
               <base-input
-                alternative
-                class="mb-3"
                 placeholder="Email"
                 addon-left-icon="ni ni-email-83"
+                :valid="validEmail"
+                v-model="formdata.email"
               ></base-input>
               <base-input
-                alternative
                 type="password"
                 placeholder="Password"
                 addon-left-icon="ni ni-lock-circle-open"
+                v-model="formdata.password"
               ></base-input>
-              <base-checkbox>Remember me</base-checkbox>
+              <base-checkbox v-model="formdata.remember">Remember me</base-checkbox>
             </form>
             <div class="mt-4">
               <v-row class="justify-center">
                 or&nbsp
                 <div class="a-register">
-                  <a>Register</a>
+                  <a @click="toggleForm()">Register</a>
                 </div>
               </v-row>
             </div>
           </div>
         </v-card-text>
         <v-card-actions class="justify-center">
-          <div class="mb-4">
-            <base-button>Login</base-button>
+          <div class="mb-10">
+            <base-button @click="login()">Login</base-button>
           </div>
         </v-card-actions>
       </v-card>
+      <!-- End of login section -->
+
+      <!-- Register section -->
+      <v-card color="blue-grey lighten-5" :loading="isLoading" v-if="!loginForm">
+        <v-card-title class="justify-center">
+          <div class="mt-4">Welcome!</div>
+        </v-card-title>
+        <v-card-text class="justify-center">
+          <div class="mt-4 mx-4">
+            <form role="form">
+              <base-input
+                placeholder="Username"
+                addon-left-icon="ni ni-circle-08"
+                :valid="validUsername"
+                v-model="formdata.username"
+              ></base-input>
+              <base-input
+                placeholder="Email"
+                addon-left-icon="ni ni-email-83"
+                :valid="validEmail"
+                v-model="formdata.email"
+              ></base-input>
+              <base-input
+                type="password"
+                placeholder="Password"
+                addon-left-icon="ni ni-lock-circle-open"
+                :valid="validPassword"
+                v-model="formdata.password"
+              ></base-input>
+              <base-checkbox v-model="formdata.remember">Remember me</base-checkbox>
+            </form>
+            <div class="mt-4">
+              <v-row class="justify-center">
+                or&nbsp
+                <div class="a-register">
+                  <a @click="toggleForm()">Login</a>
+                </div>
+              </v-row>
+            </div>
+          </div>
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <div class="mb-10">
+            <base-button @click="register()">Register</base-button>
+          </div>
+        </v-card-actions>
+      </v-card>
+      <!-- End of register section -->
     </v-dialog>
   </div>
 </template>
@@ -49,8 +98,57 @@ export default {
   name: "Login",
   data() {
     return {
-      dialog: false
+      dialog: false,
+      loginForm: true,
+      isLoading: false,
+      isLocked: false,
+      formdata: {
+        username: "",
+        email: "",
+        password: "",
+        remember: false
+      }
     };
+  },
+  computed: {
+    validUsername: function() {
+      if (this.formdata.username != "")
+        return this.formdata.username.length > 4;
+      else return null;
+    },
+    validEmail: function() {
+      if (this.formdata.email != "") {
+        var valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return valid.test(String(this.formdata.email).toLowerCase());
+      } else return null;
+    },
+    validPassword: function() {
+      if (this.formdata.password != "")
+        return this.formdata.password.length > 6;
+      else return null;
+    }
+  },
+  methods: {
+    resetForm() {
+      this.formdata.username = "";
+      this.formdata.email = "";
+      this.formdata.password = "";
+      this.formdata.remember = "";
+    },
+    toggleForm() {
+      this.resetForm();
+      this.loginForm = !this.loginForm;
+    },
+    login() {
+      this.isLoading = "blue";
+      this.isLocked = true;
+      console.log(this.formdata);
+    },
+    register() {
+      this.isLoading = "blue";
+      this.isLocked = true;
+      console.log(this.formdata);
+    }
   }
 };
 </script>
