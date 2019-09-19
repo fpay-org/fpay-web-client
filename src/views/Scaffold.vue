16a599f16c2f471e7399a7945981dd4123d24406
Sashika Nawarathne <cazci269@gmail.com>
2019-09-19 19:27:38 +0530

    Create base scaffold view

---
+++ src/views/Scaffold.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <Appbar></Appbar>
+  </v-app>
+</template>
+
+<script>
+// import BaseNav from '../components/BaseNav';
+import Appbar from "../components/shared/Appbar";
+
+export default {
+  name: "Scaffold",
+  components: {
+    // BaseNav
+    Appbar,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
