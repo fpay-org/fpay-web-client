10314737a0900228f9c77701c309c2256396b4ed
sach <sachinisiriwardhana07@gmail.com>
2020-01-15 13:22:46 -0800

    create user profile

---
+++ src/views/console/profile.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="profile">
+        <h1>User Profile</h1>
+        <p>
+            this is the user profile
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'profile',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #profile {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+    }
