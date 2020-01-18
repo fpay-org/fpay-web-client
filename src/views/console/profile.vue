17a07dc25f07738c576ca6bd95186f2ef1f131de
sachini dimalka <sachinisiriwardhana07@gmail.com>
2020-01-18 10:04:25 -0800

    Update user profile

--- src/views/console/profile.vue
+++ src/views/console/profile.vue
@@ -1,9 +1,30 @@
 <template>
     <div id="profile">
-        <h1>User Profile</h1>
-        <p>
-            this is the user profile
-        </p>
+   <div>
+    <v-app-bar color="grey accent-" dense dark>
+      <v-toolbar-title>User Profile</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <router-link to="/console">Logout</router-link>
+
+   </v-app-bar>
+  </div>
+  <h1 style="text-align:center;">fpay user profile</h1>
+
+    <v-card class="mx-auto" max-width="600">
+    <v-img height="200px" weight="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"></v-img>
+      
+    <v-card-text class="text--primary">
+    <p> User name : </p>
+    <p> Officer ID : </p>
+    <p> first name : </p>
+    <p> last name : </p>
+    <p> ploce station : </p>
+    <p> rank : </p>
+    </v-card-text>
+
+  </v-card>  
+   
     </div>
 </template>
 
@@ -11,7 +32,9 @@
     export default {
         name: 'profile',
         data() {
-            return {};
+            return {
+              
+            };
         }
     }
 </script>
@@ -23,3 +46,5 @@
         padding: 20px;
         margin-top: 10px;
     }
+    
+    
