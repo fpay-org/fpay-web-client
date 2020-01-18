56f045e7310b3114aea70b15b5ca568c748f64aa
sachini dimalka <sachinisiriwardhana07@gmail.com>
2020-01-18 12:05:25 -0800

    Implement news feed

--- src/views/console/news-feed/News.vue
+++ src/views/console/news-feed/News.vue
@@ -5,6 +5,35 @@
         <h1>
           Recent News
         </h1>
+        <div class="vertical-spacer"></div>
+
+    <div>
+      <base-dropdown title="District" class="btn-filter">
+        <div class="nav-filter" @click="onFilterAdd()">
+              Colombo
+      </div>
+      </base-dropdown>
+    </div>
+
+    <div class="vertical-spacer"></div>
+
+      <v-card
+    class="mx-auto"
+    max-width="600"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      News Headling
+    </v-card-title>
+
+    <v-card-text>
+        The gazette notice to increase fines to Rs.25,000 with regard to seven traffic offences was issued on Monday night, the Transport Ministry said.According to the gazette notice, the ministry will present the new traffic fines to parliament after amending the Motor Traffic Act.
+        </v-card-text>
+  </v-card>
       </div>
     </v-container>
   </div>
