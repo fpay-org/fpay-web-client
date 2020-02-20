2bf030ed97fa7656332608239c43537a30097770
Sashika Nawarathne <cazci269@gmail.com>
2020-02-20 12:21:45 +0530

    Update UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,37 @@
 <template>
-  <appbar></appbar>
+  <div>
+    <div class="back"></div>
+    <div class="spacer"></div>
+    <div class="login row justify-center">
+      <login></login>
+    </div>
+  </div>
 </template>
 
 <script>
-import appbar from "../components/shared/Appbar";
+import login from "../components/dialogs/Login";
 
 export default {
   name: "home",
   components: {
-    appbar
+    login
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.back {
+  position: absolute;
+  width: 100%;
+  height: 400px;
+  background-color: #003b46;
+}
+
+.spacer {
+  height: 380px;
+}
+
+.spacer-bottom {
+  height: 200px;
+}
+</style>
